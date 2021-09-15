2b0b4772d2da62dcd12d3f1f62b02df8da61099e
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-09-15 12:52:41 +0200

    Add direnv

--- users/pg/lorri.nix
+++ users/pg/lorri.nix
@@ -1,1 +1,11 @@
-{ config, pkgs, ... }: { services.lorri.enable = true; }
+{ config, pkgs, ... }: {
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv = {
+      enable = true;
+      enableFlakes = true;
+    };
+  };
+  services.lorri.enable = true;
+}
