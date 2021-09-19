cdeb13feffb1835c705d195b302bb69f8037902e
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-09-19 10:02:23 +0200

    Set up SSH server

--- hosts/base.nix
+++ hosts/base.nix
@@ -66,11 +66,27 @@
     enableSSHSupport = false;
   };
   programs.ssh.startAgent = true;
+  services.openssh = {
+    enable = true;
+    allowSFTP = false;
+    passwordAuthentication = false;
+    permitRootLogin = "no";
+    listenAddresses = [{
+      addr = "100.79.65.104";
+      port = 22;
+    }];
+  };
+  users.mutableUsers = false;
   users.users.pg = {
     isNormalUser = true;
     extraGroups = [ "wheel" "video" "libvirtd" "podman" ];
     shell = pkgs.zsh;
     description = "Patryk Gronkiewicz";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJQfo481KqK1LXCwZdz6sDvEbgxoL0cqV3n0J+nbTjaZ u0_a313@localhost"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDH7219zlzbwGZxSHkbKWFZ6HlgYF352/88uyhapeMNRAAAABHNzaDo= pg@artemis"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAVmIuEGVOre3GqyZydZiZ/BKYFLSLqU9qZLmIn6BN1ZAAAABHNzaDo= pg@artemis"
+    ];
   };
   time.timeZone = "Europe/Warsaw";
   nixpkgs.config.allowUnfree = true;
