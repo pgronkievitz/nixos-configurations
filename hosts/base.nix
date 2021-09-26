0de2c6fee9965036e92867e9507010cfd5f3e0bc
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-09-26 22:46:07 +0200

    Update emacs-overlay

--- hosts/base.nix
+++ hosts/base.nix
@@ -103,7 +103,7 @@
   nixpkgs.overlays = [
     (import (builtins.fetchGit {
       url = "https://github.com/nix-community/emacs-overlay.git";
-      rev = "2ff125e11371b88b1c4edeaa6f96355fbfee96da";
+      rev = "44b437b1d6f8b4ac6720d56827e7d802eef7f59e";
     }))
   ];
 }
