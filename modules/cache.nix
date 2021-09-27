02f7e0dca22ca721229c5224b5075d8287c71677
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-09-27 06:54:02 +0200

    Add binary cache for NixOS repo

--- modules/cache.nix
+++ modules/cache.nix
@@ -1,7 +1,11 @@
 {
   nix = {
-    binaryCaches = [ "https://nix-community.cachix.org" ];
+    binaryCaches = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "https://nix-community.cachix.org"
+    ];
     binaryCachePublicKeys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
