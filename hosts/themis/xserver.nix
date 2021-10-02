4bd07cf425ec79a75b9b4a6a89e304626bd64dec
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-10-02 15:56:00 +0200

    Update setup for nvidia

--- hosts/themis/xserver.nix
+++ hosts/themis/xserver.nix
@@ -1,7 +1,16 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
+in {
   services.xserver = {
     enable = true;
-    videoDrivers = [ "nvidia" "amdgpu" ];
+    videoDrivers = [ "modesetting" "nvidia" ];
     desktopManager = { xterm.enable = false; };
     displayManager = {
       sddm.enable = true;
@@ -11,5 +20,10 @@
     layout = "pl";
     xkbOptions = "caps:swapescape";
     libinput.enable = true;
+  };
+  hardware.nvidia.prime = {
+    offload.enable = true;
+    amdgpuBusId = "PCI:05:00:0";
+    nvidiaBusId = "PCI:01:00:0";
   };
 }
