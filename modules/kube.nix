40264746ac3bc57be593a2f857e0be33a273af59
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-10-24 16:48:40 +0200

    Comment out k3s service

--- modules/kube.nix
+++ modules/kube.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [ kube3d k3s minikube kubectl ];
-  services.k3s = {
-    enable = true;
-    role = "server";
-  };
+  # services.k3s = {
+  #   enable = true;
+  #   role = "server";
+  # };
 }
