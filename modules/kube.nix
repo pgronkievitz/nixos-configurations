6f22d612f319b4da15eb294d18e3753285e84938
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-10-31 15:26:54 +0100

    Add kompose to kube

--- modules/kube.nix
+++ modules/kube.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ kubectl kubernetes-helm ];
+  environment.systemPackages = with pkgs; [ kubectl kubernetes-helm kompose ];
   # services.k3s = {
   #   enable = true;
   #   role = "server";
