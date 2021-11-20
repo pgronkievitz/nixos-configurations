2efb4a3a3e01a53fb97993140825b5157e215948
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-11-20 23:15:56 +0100

    Define variables to prevent diverging

--- modules/selfhosted/vaultwarden.nix
+++ modules/selfhosted/vaultwarden.nix
@@ -1,4 +1,8 @@
-{
+let
+  servicename = "vaultwarden";
+  shortname = "vault";
+  port = 9001;
+in {
   virtualisation.oci-containers = {
     backend = "podman";
     containers = { vaultwarden = { image = ""; }; };
