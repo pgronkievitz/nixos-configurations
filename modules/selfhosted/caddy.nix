fa9f44108d287ecb55597d0acd67f0b3f7d2e52e
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-11-20 23:16:38 +0100

    Separate VH and read config from file

--- modules/selfhosted/caddy.nix
+++ modules/selfhosted/caddy.nix
@@ -2,18 +2,6 @@
   services.caddy = {
     enable = true;
     email = "caddy@gronkiewicz.xyz";
-    config = ''
-      {
-        acme_dns cloudflare aaaaaaa
-      }
-    '';
-    virtualHosts = {
-      "vault.gronkiewicz.xyz" = {
-        serverAliases = [ "www.vault.gronkiewicz.xyz" ];
-        extraConfig = ''
-          reverse_proxy 9001
-        '';
-      };
-    };
+    config = builtins.readFile ./Caddyfile;
   };
 }
