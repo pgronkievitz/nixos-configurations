c17aef9e2e80b6bd46ae5ddbc56f1df95ffea5f9
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-11-20 23:17:34 +0100

    Define oci container for vaultwarden

--- modules/selfhosted/vaultwarden.nix
+++ modules/selfhosted/vaultwarden.nix
@@ -4,6 +4,19 @@ let
   port = 9001;
 in {
   virtualisation.oci-containers = {
-    containers = { vaultwarden = { image = ""; }; };
+    containers = {
+      "${servicename}" = {
+        image = "vaultwarden/server:1.23.0-alpine";
+        volumes = [ "/media/data/${servicename}:/data" ];
+        ports = [ "${port}:80" ];
+        environment = {
+          WEBSOCKET_ENABLED = "true";
+          DOMAIN = "https://${shortname}.gronkiewicz.xyz";
+          SIGNUPS_ALLOWED = "false";
+        };
+        user = "${servicename}:services";
+      };
+    };
+  };
   };
 }
