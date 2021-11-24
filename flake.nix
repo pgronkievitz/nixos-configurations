4fbad3299a9945b1714dfc47c0b306f7911099e5
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-11-24 15:01:57 +0100

    Make emacs-overlay use master branch again

--- flake.nix
+++ flake.nix
@@ -5,8 +5,7 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    emacs-overlay.url =
-      "github:nix-community/emacs-overlay/23c8464f4527a2b19f6b4776378dd03b8289aa85";
+    emacs-overlay.url = "github:nix-community/emacs-overlay/master";
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }:
