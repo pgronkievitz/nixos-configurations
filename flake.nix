9c9230a1ad0a3e82fddf8eca1e7912c9904eae5c
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-11-29 21:53:26 +0100

    Reformat home-manager to fit in one block

--- flake.nix
+++ flake.nix
@@ -3,8 +3,10 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     emacs-overlay.url = "github:nix-community/emacs-overlay/master";
   };
 
