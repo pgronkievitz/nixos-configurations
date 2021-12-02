cecf6b61f72546fd63974722c2d32d0e7412bb47
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-12-02 11:49:14 +0100

    Move gitea service to pg user

--- modules/selfhosted/gitea.nix
+++ modules/selfhosted/gitea.nix
@@ -5,7 +5,7 @@
       gitea = {
         image = "docker.io/gitea/gitea:1.15.6-rootless";
         ports = [ "8081:3000" ];
-        user = "1000:1000";
+        user = "pg";
         login = {
           username = "pgronkievitz";
           registry = "https://docker.io";
