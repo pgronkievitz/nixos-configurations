92c6290a97dd86308744c7df794a182ab5681741
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-12-02 23:02:23 +0100

    Add nose and pipenv
    Just to mitigate doom doctor warnings

--- modules/development/python.nix
+++ modules/development/python.nix
@@ -8,9 +8,10 @@ let
     python-packages.flake8
     python-packages.pylint
     python-packages.mypy
+    python-packages.nose
   ];
   python-with-my-packages = pkgs.python3.withPackages my-python-packages;
 in {
   environment.systemPackages =
-    [ python-with-my-packages pkgs.black pkgs.pyright ];
+    [ python-with-my-packages pkgs.pipenv pkgs.black pkgs.pyright ];
 }
