1fdca43e38fef6483fb76ff27d7c85109f6f81f6
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-12-02 11:42:47 +0100

    Change gitea to default user

--- modules/selfhosted/gitea.nix
+++ modules/selfhosted/gitea.nix
@@ -5,7 +5,7 @@
       gitea = {
         image = "docker.io/gitea/gitea:1.15.6-rootless";
         ports = [ "8081:3000" ];
-        user = "podman";
+        user = "1000:1000";
         login = {
           username = "pgronkievitz";
           registry = "https://docker.io";
