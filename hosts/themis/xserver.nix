bd43177b48103f499f2e021e4f64372d507d1cae
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-12-03 09:48:02 +0100

    Enable modesetting for nvidia
    It fixes tearing

--- hosts/themis/xserver.nix
+++ hosts/themis/xserver.nix
@@ -1,1 +1,4 @@
-{ config, lib, pkgs, ... }: { services.xserver.videoDrivers = [ "nvidia" ]; }
+{ config, lib, pkgs, ... }: {
+  hardware.nvidia.modesetting.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+}
