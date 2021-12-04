6c3d177aedefa9b2c19a13fc6cc64a1bda909b03
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-12-04 20:31:55 +0100

    Set up wallabag

--- modules/selfhosted/wallabag.nix
+++ modules/selfhosted/wallabag.nix
@@ -2,11 +2,16 @@ let
   servicename = "wallabag";
   shortname = "wb";
   port = "9201";
+  domains = [
+    "www.${shortname}.gronkiewicz.xyz"
+    "${servicename}.gronkiewicz.xyz"
+    "www.${servicename}.gronkiewicz.xyz"
+  ];
 in {
   virtualisation.oci-containers = {
     containers = {
       "${servicename}" = {
-        image = "docker.io/wallabag/wallabag:2.4.2";
+        image = "wallabag/wallabag:2.4.2";
         volumes = [ "/media/data/${servicename}:/var/www/wallabag/data" ];
         ports = [ "${port}:80" ];
         environment = {
@@ -18,18 +23,15 @@ in {
   };
   services.nginx.virtualHosts = {
     "${shortname}.gronkiewicz.xyz" = {
-      enableACME = true;
+      useACMEHost = "${shortname}.gronkiewicz.xyz";
       forceSSL = true;
-      locations."/" = {
-        proxyPass = "https://127.0.0.1:${port}";
-        extraConfig = "proxy_ssl_server_name on;"
-          + "proxy_pass_header Authorization;";
-      };
-      serverAliases = [
-        "www.${shortname}.gronkiewicz.xyz"
-        "${servicename}.gronkiewicz.xyz"
-        "www.${servicename}.gronkiewicz.xyz"
-      ];
+      locations."/".proxyPass = "http://127.0.0.1:${port}";
+      serverAliases = domains;
     };
+  };
+  security.acme.certs."${shortname}.gronkiewicz.xyz" = {
+    extraDomainNames = domains;
+    dnsProvider = "cloudflare";
+    credentialsFile = "/home/pg/credentials.sh";
   };
 }
