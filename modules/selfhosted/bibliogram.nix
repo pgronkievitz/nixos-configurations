182bf6a58a5da345b1bf60c47395e4dbb65dedb6
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-12-04 18:57:27 +0100

    Fix bibliogram

--- modules/selfhosted/bibliogram.nix
+++ modules/selfhosted/bibliogram.nix
@@ -2,11 +2,16 @@ let
   servicename = "bibliogram";
   shortname = "bib";
   port = "9301";
+  domains = [
+    "www.${shortname}.gronkiewicz.xyz"
+    "${servicename}.gronkiewicz.xyz"
+    "www.${servicename}.gronkiewicz.xyz"
+  ];
 in {
   virtualisation.oci-containers = {
     containers = {
       "${servicename}" = {
-        image = "docker.io/cloudrac3r/bibliogram:latest";
+        image = "cloudrac3r/bibliogram:latest";
         ports = [ "${port}:10407" ];
         environment = {
           "SYMFONY__ENV__DOMAIN_NAME" =
@@ -18,18 +23,15 @@ in {
   };
   services.nginx.virtualHosts = {
     "${shortname}.gronkiewicz.xyz" = {
-      enableACME = true;
+      useACMEHost = "${shortname}.gronkiewicz.xyz";
       forceSSL = true;
-      locations."/" = {
-        proxyPass = "https://127.0.0.1:${port}";
-        extraConfig = "proxy_ssl_server_name on;"
-          + "proxy_pass_header Authorization;";
-      };
-      serverAliases = [
-        "www.${shortname}.gronkiewicz.xyz"
-        "${servicename}.gronkiewicz.xyz"
-        "www.${servicename}.gronkiewicz.xyz"
-      ];
+      locations."/".proxyPass = "http://127.0.0.1:${port}";
+      serverAliases = domains;
     };
+  };
+  security.acme.certs."${shortname}.gronkiewicz.xyz" = {
+    extraDomainNames = domains;
+    dnsProvider = "cloudflare";
+    credentialsFile = "/home/pg/credentials.sh";
   };
 }
