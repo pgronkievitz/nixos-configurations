4d30a622e6729c2e1e413b1d3bd46ceaab80635b
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-12-07 21:45:54 +0100

    Change UTF-8 to utf8

--- home/localization.nix
+++ home/localization.nix
@@ -4,17 +4,17 @@
     options = [ "caps:swapescape" ];
   };
   home.language = {
-    address = "pl_PL.UTF-8";
-    base = "en_US.UTF-8";
-    collate = "pl_PL.UTF-8";
-    ctype = "pl_PL.UTF-8";
-    measurement = "pl_PL.UTF-8";
-    messages = "en_US.UTF-8";
-    monetary = "pl_PL.UTF-8";
-    name = "pl_PL.UTF-8";
-    numeric = "pl_PL.UTF-8";
-    paper = "pl_PL.UTF-8";
-    telephone = "pl_PL.UTF-8";
-    time = "pl_PL.UTF-8";
+    address = "pl_PL.utf8";
+    base = "en_US.utf8";
+    collate = "pl_PL.utf8";
+    ctype = "pl_PL.utf8";
+    measurement = "pl_PL.utf8";
+    messages = "en_US.utf8";
+    monetary = "pl_PL.utf8";
+    name = "pl_PL.utf8";
+    numeric = "pl_PL.utf8";
+    paper = "pl_PL.utf8";
+    telephone = "pl_PL.utf8";
+    time = "pl_PL.utf8";
   };
 }
