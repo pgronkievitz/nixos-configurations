415159491f1d68ce971e04199870d6713c603b29
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-12-07 21:43:15 +0100

    Fix language settings

--- home/localization.nix
+++ home/localization.nix
@@ -4,17 +4,17 @@
     options = [ "caps:swapescape" ];
   };
   home.language = {
-    address = "pl";
-    base = "us";
-    collate = "pl";
-    ctype = "pl";
-    measurement = "pl";
-    messages = "us";
-    monetary = "pl";
-    name = "pl";
-    numeric = "pl";
-    paper = "pl";
-    telephone = "pl";
-    time = "pl";
+    address = "pl_PL.UTF-8";
+    base = "en_US.UTF-8";
+    collate = "pl_PL.UTF-8";
+    ctype = "pl_PL.UTF-8";
+    measurement = "pl_PL.UTF-8";
+    messages = "en_US.UTF-8";
+    monetary = "pl_PL.UTF-8";
+    name = "pl_PL.UTF-8";
+    numeric = "pl_PL.UTF-8";
+    paper = "pl_PL.UTF-8";
+    telephone = "pl_PL.UTF-8";
+    time = "pl_PL.UTF-8";
   };
 }
