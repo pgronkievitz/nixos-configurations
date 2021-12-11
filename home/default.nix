bb7a99c5642e03566695fb16786af619cf09b39e
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-12-11 11:45:52 +0100

    Fix cursor name

--- home/default.nix
+++ home/default.nix
@@ -21,7 +21,7 @@
   };
   xsession.pointerCursor = {
     package = pkgs.bibata-cursors;
-    name = "Bibata Classic";
+    name = "Bibata_Classic";
   };
   services.udiskie = {
     enable = true;
