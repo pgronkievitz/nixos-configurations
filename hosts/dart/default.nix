39bc03ac7eb046ae684ca2b8bfdb7ca78570bc66
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-12-18 22:43:08 +0100

    add hostId

--- hosts/dart/default.nix
+++ hosts/dart/default.nix
@@ -1,8 +1,9 @@
-{ ... }:
-{
+{ ... }: {
   imports = [ ./hardware-configuration.nix ./backups.nix ];
+
   networking = {
     hostName = "dart";
+    hostId = "39a39d05";
   };
   hardware.bluetooth.enable = false;
 }
