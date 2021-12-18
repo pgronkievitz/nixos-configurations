b1f0e8606fb0ca7638272db0ffe47b6c68d4480c
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-12-18 23:02:35 +0100

    add mountpoints for zfs

--- hosts/dart/hardware-configuration.nix
+++ hosts/dart/hardware-configuration.nix
@@ -4,24 +4,31 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules =
+    [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-label/ROOT";
-      fsType = "btrfs";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/ROOT";
+    fsType = "btrfs";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-label/BOOT";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/BOOT";
+    fsType = "vfat";
+  };
+  fileSystems."/media/data" = {
+    device = "data/selfhosted";
+    fsType = "zfs";
+  };
+  fileSystems."/media/backups" = {
+    device = "data/backups";
+    fsType = "zfs";
+  };
 
   swapDevices = [ ];
 
