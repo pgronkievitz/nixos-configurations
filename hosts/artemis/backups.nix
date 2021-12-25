75843278cf93d5707dd9284c88bd6bb7b5896bcb
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-12-25 23:05:43 +0100

    Replace local backups with rest server
    I don't have secondary drive and I own rest server anyways, so it
    doesn't really matter

--- hosts/artemis/backups.nix
+++ hosts/artemis/backups.nix
@@ -23,25 +23,28 @@
       timerConfig = { OnCalendar = "0/4:00"; };
       environmentFile = config.age.secrets.artemisbkp-env.path;
     };
-    # local = {
-    #   user = "pg";
-    #   repository = "/media/pg/ext";
-    #   initialize = true;
-    #   passwordFile = config.age.secrets.artemisbkp.path;
-    #   paths = [ "/home/pg" ];
-    #   extraBackupArgs = [
-    #     "--exclude-caches"
-    #     "--exclude=/home/pg/VM"
-    #     "--exclude=/home/pg/Videos"
-    #     "--exclude=/home/pg/Music"
-    #   ];
-    #   pruneOpts = [
-    #     "--keep-daily 24"
-    #     "--keep-weekly 3"
-    #     "--keep-monthly 12"
-    #     "--keep-yearly 10"
-    #   ];
-    #   timerConfig = { OnCalendar = "hourly"; };
-    # };
+    rest = {
+      user = "pg";
+      repository = "rest:https://restic.gronkiewicz.xyz/pg";
+      initialize = true;
+      passwordFile = config.age.secrets.artemisbkp.path;
+      paths = [ "/home/pg" ];
+      extraBackupArgs = [
+        "--exclude-caches"
+        "--exclude=/home/pg/VM"
+        "--exclude=/home/pg/Videos"
+        "--exclude=/home/pg/Music"
+      ];
+      pruneOpts = [
+        "--keep-daily 4"
+        "--keep-weekly 3"
+        "--keep-monthly 12"
+        "--keep-yearly 10"
+      ];
+      timerConfig = {
+        OnCalendar = "0/2:00";
+        RandomizedDelaySec = "30m";
+      };
+    };
   };
 }
