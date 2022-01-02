79e340665bae7d004870b9ca4594104d8028921d
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-01-02 21:18:10 +0100

    Fix CA

--- modules/selfhosted/ca.nix
+++ modules/selfhosted/ca.nix
@@ -1,9 +1,12 @@
-{ config, ... }: {
-  services.nginx.virtualHosts."ca.gronkiewicz.xyz" = {
+{ pkgs, config, ... }:
+let shortname = "ca";
+in {
+  services.nginx.virtualHosts."${shortname}.gronkiewicz.xyz" = {
     addSSL = true;
-    root = "/var/www/ca.gronkiewicz.xyz";
-    sslCertificateKey = "/media/data/certs/ca/key.pem";
-    sslCertificate = "/media/data/certs/ca/crt.pem";
+    root = "/var/www/${shortname}.gronkiewicz.xyz";
+    sslCertificateKey = "/media/data/certs/${shortname}/key.pem";
+    sslCertificate = "/media/data/certs/${shortname}/crt.pem";
     extraConfig = "ssl_password_file ${config.age.secrets.ssl.path};";
   };
+  environment.systemPackages = [ pkgs.openssl pkgs.easyrsa ];
 }
