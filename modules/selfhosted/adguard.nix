66a63cce99370bee05d6030b8174824f327d1fbf
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-01-02 14:56:14 +0100

    Use AdGuard via docker

--- modules/selfhosted/adguard.nix
+++ modules/selfhosted/adguard.nix
@@ -4,10 +4,30 @@ let
   port = 10001;
   domains = [ "${servicename}.gronkiewicz.xyz" ];
 in { config, ... }: {
-  services.adguardhome = {
-    enable = true;
-    port = port;
-    host = "0.0.0.0";
+  virtualisation.oci-containers = {
+    containers = {
+      "${servicename}" = {
+        image = "adguard/adguardhome:v0.107.2";
+        ports = [
+          "${port}:80"
+          "3000:3000/tcp"
+          # regular DNS
+          "53:53/tcp"
+          "53:53/udp"
+          # DNSCrypt
+          "5443:5443/tcp"
+          "5443:5443/udp"
+          # DoQ
+          "784:784/udp"
+          "853:853/udp"
+          "8853:8853/udp"
+          # DoT
+          "853:853/tcp"
+        ];
+        volumes = [ "/media/data/${servicename}:/opt/adguardhome" ];
+        environment = { TZ = "Europe/Warsaw"; };
+      };
+    };
   };
   services.nginx.virtualHosts = {
     "${shortname}.gronkiewicz.xyz" = {
