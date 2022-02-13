8ab0c522aa5d992f2cf46daf2216ef9f0bf69584
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-02-13 19:31:19 +0100

    Add homer on hubble
    Add homer

---
+++ modules/selfhosted/homer.nix
@@ -0,0 +1,17 @@
+let
+  servicename = "homer";
+  shortname = "dash";
+in { config, ... }: {
+  virtualisation.oci-containers = {
+    containers = {
+      "${servicename}" = {
+        image = "b4bz/homer:22.02.1";
+        volumes = [ "/media/data/${servicename}:/www/assets" ];
+        extraOptions = [
+          "--label=traefik.http.routers.${servicename}.rule=Host(`${shortname}.gronkiewicz.xyz`,`${shortname}.lab.home`)"
+          "--label=traefik.http.routers.${servicename}.tls=true"
+        ];
+      };
+    };
+  };
+}
