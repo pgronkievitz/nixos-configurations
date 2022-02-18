0628e56b7666e356901e8f366b3f479e1ca16b69
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-02-18 20:35:13 +0100

    Prepare nextcloud for companion containers

--- modules/selfhosted/nextcloud.nix
+++ modules/selfhosted/nextcloud.nix
@@ -6,10 +6,11 @@ in { config, ... }: {
     containers = {
       "${servicename}" = {
         image = "nextcloud:23.0.1";
-        volumes = [ "/media/data/${servicename}:/var/www/html" ];
+        volumes = [ "/media/data/${servicename}/data:/var/www/html" ];
         extraOptions = [
           "--label=traefik.http.routers.${servicename}.rule=Host(`${shortname}.gronkiewicz.xyz`,`${shortname}.lab.home`)"
           "--label=traefik.http.routers.${servicename}.tls=true"
+          "--network=nextcloud"
         ];
       };
     };
