26393460eedcfeaddf3b9ef02890761b41c1f881
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-02-19 13:10:19 +0100

    Update fonts config

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,9 +1,19 @@
 { pkgs, ... }: {
-  fonts.fonts = [
-    (pkgs.nerdfonts.override {
-      fonts = [ "UbuntuMono" "FiraCode" "VictorMono" ];
-    })
-    pkgs.merriweather
-    pkgs.merriweather-sans
-  ];
+  fonts = {
+    fonts = [
+      (pkgs.nerdfonts.override {
+        fonts = [ "Ubuntu" "UbuntuMono" "FiraCode" "VictorMono" ];
+      })
+      pkgs.merriweather
+      pkgs.merriweather-sans
+    ];
+    fontconfig = {
+      defaultFonts = {
+        serif = [ "Merriweather" ];
+        emoji = [ "Noto Color Emoji" ];
+        monospace = [ "VictorMono Nerd Font" ];
+        sansSerif = [ "Ubuntu Nerd Font" ];
+      };
+    };
+  };
 }
