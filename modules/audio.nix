f6086a6d574e7785be46d87b4413ec3224019800
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-03-15 21:54:37 +0100

    remove/audio: remove media-session rules

--- modules/audio.nix
+++ modules/audio.nix
@@ -9,21 +9,5 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-    media-session.config.bluez-monitor.rules = [
-      {
-        matches = [{ "device.name" = "~bluez_card.*"; }];
-        actions."update-props" = {
-          "bluez5.reconnect-profiles" = [ "a2dp_sink" ];
-          "bluez5.codecs" = [ "aac" ];
-        };
-      }
-      {
-        matches = [
-          { "node.name" = "~bluez_input.*"; }
-          { "node.name" = "~bluez_output.*"; }
-        ];
-        actions."node.pause-on-idle" = false;
-      }
-    ];
   };
 }
