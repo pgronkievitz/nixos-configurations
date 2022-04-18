12ea4a11529e10839aeb582ec8ca7ac20ab6d801
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-04-18 15:18:56 +0200

    update/podsync: update volumes

--- modules/selfhosted/podsync.nix
+++ modules/selfhosted/podsync.nix
@@ -6,7 +6,7 @@ in { config, ... }: {
     containers = {
       "${servicename}" = {
         image = "mxpv/podsync:v2.4.2";
-        volumes = [ "/media/data/${servicename}:/app" ];
+        volumes = [ "/media/data/${servicename}:/app/data" "/media/data/${servicename}/config.toml:/app/config.toml"];
         extraOptions = [
           "--label=traefik.http.routers.${servicename}.rule=Host(`${shortname}.gronkiewicz.xyz`,`${shortname}.lab.home`)"
           "--label=traefik.http.routers.${servicename}.tls=true"
