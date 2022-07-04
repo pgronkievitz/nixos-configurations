59ed7f587aab01450926909a05b226c07a66439c
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-07-04 13:48:55 +0200

    fix/kiwix: add cmd

--- modules/selfhosted/kiwix.nix
+++ modules/selfhosted/kiwix.nix
@@ -13,6 +13,7 @@ in { config, ... }: {
           "--label=traefik.http.routers.${servicename}.rule=Host(`${shortname}.gronkiewicz.xyz`,`${shortname}.lab.home`)"
           "--label=traefik.http.routers.${servicename}.tls=true"
         ];
+        cmd = ["wikipedia_en_all_maxi_2022-05.zim"];
       };
     };
   };
