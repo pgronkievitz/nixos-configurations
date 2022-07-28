8acd9ce8c3bdf0b9adce8b18f425c084e9266149
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-07-28 21:16:40 +0200

    add/minio: metrics

--- modules/selfhosted/minio.nix
+++ modules/selfhosted/minio.nix
@@ -5,4 +5,8 @@
     enable = true;
     dataDir = ["/media/data/minio"];
   };
+  services.prometheus.exporters.minio = {
+    enable = true;
+    minioAddress = "http://100.95.47.112:9000";
+  };
 }
