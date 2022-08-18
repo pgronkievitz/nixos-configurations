9e973a6c2036de7699aabf37333865586391d23d
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-08-18 22:40:25 +0200

    update/znc: add port fwd

--- modules/selfhosted/znc.nix
+++ modules/selfhosted/znc.nix
@@ -7,6 +7,9 @@ in { config, ... }: {
       "${servicename}" = {
         image = "znc:1.8.2";
         volumes = [ "/media/data/${servicename}:/znc-data" ];
+        ports = [
+          "5000:5000"
+        ];
         extraOptions = [
           "--label=traefik.http.routers.${servicename}.rule=Host(`${shortname}.gronkiewicz.xyz`,`${shortname}.lab.home`)"
           "--label=traefik.http.services.${servicename}.loadbalancer.server.port=5000"
