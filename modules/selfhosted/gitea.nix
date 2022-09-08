9cda2d8dbf76d7c3adffc3018f63e35a2981b8cb
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-09-08 22:45:30 +0200

    fix/gitea: ssl

--- modules/selfhosted/gitea.nix
+++ modules/selfhosted/gitea.nix
@@ -10,7 +10,6 @@ in { config, ... }: {
         extraOptions = [
           "--label=traefik.http.routers.${servicename}.rule=Host(`${shortname}.gronkiewicz.dev`,`${shortname}.lab.home`)"
           "--label=traefik.http.services.${servicename}.loadbalancer.server.port=3000"
-          "--label=traefik.http.routers.${servicename}.tls=true"
         ];
         ports = [ "22:22" ];
       };
