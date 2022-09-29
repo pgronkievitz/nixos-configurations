4a106d25c1e970327d88e7643ea6dd9d5e2e8bb6
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-09-29 13:34:06 +0200

    update/traefik

--- modules/selfhosted/traefik.nix
+++ modules/selfhosted/traefik.nix
@@ -7,10 +7,10 @@
       providers.docker.exposedbydefault = true;
       entryPoints.http = {
         address = ":80";
-        # http.redirections.entryPoint = {
-        #   to = "https";
-        #   scheme = "https";
-        # };
+        http.redirections.entryPoint = {
+          to = "https";
+          scheme = "https";
+        };
       };
       entryPoints.https.address = ":443";
       certificatesResolvers.letsencrypt.acme = {
@@ -22,10 +22,12 @@
       metrics.prometheus.addRoutersLabels = true;
     };
     dynamicConfigOptions = {
-      tls.stores.default.defaultCertificate = {
-        certFile = "/media/data/ca/cert.pem";
-        keyFile = "/media/data/ca/key.pem";
-      };
+      tls.certificates = [
+        {
+          certFile = "/media/data/ca/cert.pem";
+          keyFile = "/media/data/ca/key.pem";
+        }
+      ];
     };
   };
   networking.firewall.allowedTCPPorts = [ 80 443 ];
