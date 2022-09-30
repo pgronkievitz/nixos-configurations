39c991897d67b88483be939836126fbd9a22d25f
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-09-30 23:33:56 +0200

    move/gui shit

--- home/default.nix
+++ home/default.nix
@@ -1,46 +1,7 @@
 { pkgs, ... }: {
   imports = [ ./cli ./shell ./localization.nix ];
-  gtk = {
-    enable = true;
-    iconTheme = {
-      package = pkgs.flat-remix-icon-theme;
-      name = "Flat-Remix-Blue-Light";
-    };
-    theme = {
-      package = pkgs.plano-theme;
-      name = "Plano";
-    };
-    cursorTheme = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Classic";
-      size = 24;
-    };
-  };
   systemd.user.sessionVariables.EDITOR = "emacs";
   home.sessionVariables.EDITOR = "emacs";
-  services.pasystray.enable = true;
-  services.picom = {
-    enable = true;
-    fade = true;
-    fadeDelta = 5;
-    package = pkgs.picom-dccsillag;
-  };
-  home.pointerCursor = {
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
-    size = 24;
-    gtk.enable = true;
-    x11.enable = true;
-  };
-  services.udiskie = {
-    enable = true;
-    automount = true;
-    notify = true;
-  };
-  services.betterlockscreen = {
-    enable = true;
-    arguments = [ "--dim" "20" "--blur" "0.3" "--wall" ];
-  };
   xdg.userDirs = {
     enable = true;
     createDirectories = true;
--- home/xserver.nix
+++ home/xserver.nix
@@ -21,4 +21,43 @@
     pkgs.maim
     pkgs.numlockx
   ];
+  gtk = {
+    enable = true;
+    iconTheme = {
+      package = pkgs.flat-remix-icon-theme;
+      name = "Flat-Remix-Blue-Light";
+    };
+    theme = {
+      package = pkgs.plano-theme;
+      name = "Plano";
+    };
+    cursorTheme = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
+      size = 24;
+    };
+  };
+  services.pasystray.enable = true;
+  services.picom = {
+    enable = true;
+    fade = true;
+    fadeDelta = 5;
+    package = pkgs.picom-dccsillag;
+  };
+  home.pointerCursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+  services.udiskie = {
+    enable = true;
+    automount = true;
+    notify = true;
+  };
+  services.betterlockscreen = {
+    enable = true;
+    arguments = [ "--dim" "20" "--blur" "0.3" "--wall" ];
+  };
 }
